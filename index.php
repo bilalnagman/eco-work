14abe966e3a2cb6869d37e89271336cace7e3539
Bilal Nagman <bilalnagman.official@gmail.com>
2023-08-22 18:36:38 +0400

    Login Page bug UI LU01

--- index.php
+++ index.php
@@ -10,34 +10,43 @@
 </head>
 <body>
     
-<nav class="navbar navbar-expand-lg bg-primary fixed-top">
+<nav class="navbar navbar-expand-lg bg-primary fixed-top rounded-5 rounded-top-0">
   <div class="container">
     <a class="navbar-brand" href="#">Navbar</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon "></span>
+      <span class="bi bi-list text-light "></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-5">
-        <li class="nav-item">
+    <div class="collapse navbar-collapse " id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0 ms-lg-5 d-flex justify-content-center">
+        <li class="nav-item d-flex justify-content-center">
           <a class="nav-link active" aria-current="page" href="#">Home</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li class="nav-item d-flex justify-content-center">
+          <a class="nav-link" href="#">page 1</a>
         </li>
-      </ul>
-
-      <div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-      Dropdown button
-    </button>
-    <ul class="dropdown-menu">
-      <li><a class="dropdown-item" href="#">Action</a></li>
-      <li><a class="dropdown-item" href="#">Another action</a></li>
-      <li><a class="dropdown-item" href="#">Something else here</a></li>
-    </ul>
-  </div>
-    </div>
-
+        <li class="nav-item d-flex justify-content-center">
+          <a class="nav-link" href="#">page 2</a>
+        </li>
+        <li class="nav-item d-flex justify-content-center">
+          <a class="nav-link" href="#">page 3</a>
+        </li>
+        <li class="nav-item d-flex justify-content-center">
+          <a class="nav-link" href="#">page 5</a>
+        </li>
+        </ul>
+        <div class="dropdown">
+         <button class="btn btn-outline-light" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+           More<span class="bi bi-arrow-down-short text-light "></span>
+         </button>
+         <ul class="dropdown-menu">
+           <li><a class="dropdown-item hover-overlay" href="#">Login</a></li>
+           <li><a class="dropdown-item" href="#">Request to Sign-in </a></li>
+           <li><a class="dropdown-item" href="#">Subscripion pricing</a></li>
+         </ul>
+       </div>
+      </div>
+      
+    
 
 
   </div>
@@ -54,15 +63,21 @@
 
 
 
-  <section class="vh-100">
+  <section class="vh-100 align-items-center">
   <div class="container-fluid h-custom">
     <div class="row d-flex justify-content-center align-items-center h-100">
-      <div   class="col-md-9 col-lg-6 col-xl-5 ">
+      
+    
+      
+
+    <div   class="d-none d-lg-block col-md-9 col-lg-6 col-xl-5 d-flex  justify-content-center">
         <img   src="./resources/img/png/auditors-comptroller.png"
-          class="img-fluid user-select-none" alt="Sample image">
+          class="img-fluid user-select-none " style="wigth: 500px;" alt="Sample image">
       </div>
-      <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-        <form>
+
+
+      <div class="col-md-8 col-lg-6 col-xl-4 ">
+      <form>
         
 
           <div class="divider d-flex align-items-center my-4">
@@ -109,33 +124,17 @@
       </div>
     </div>
   </div>
-  <div
-    class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
+  
+</section>
+<div
+    class="text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-dark fixed-bottom">
     <!-- Copyright -->
-    <div class="text-white mb-3 mb-md-0">
+    <div class="text-white mb-2 mb-md-0">
       Copyright © 2023. All rights reserved by Bilal ahmed.
     </div>
     <!-- Copyright -->
 
-    <!-- Right -->
-    <div>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-twitter"></i>
-      </a>
-      <a href="#!" class="text-white me-4">
-        <i class="fab fa-google"></i>
-      </a>
-      <a href="#!" class="text-white">
-        <i class="fab fa-linkedin-in"></i>
-      </a>
-    </div>
-    <!-- Right -->
   </div>
-</section>
-
 
 <!-- Link to Bootstrap JavaScript and jQuery -->
 <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
